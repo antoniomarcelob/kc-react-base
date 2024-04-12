340a4bdd1a76e608ff2bfbfcf8e2d8a2b6f5c84b
Antonio Marcelo <antoniomarcelob12@gmail.com>
2024-04-12 14:15:30 -0300

    feat: add environment variables

--- src/auth/kc-config.ts
+++ src/auth/kc-config.ts
@@ -1,10 +1,10 @@
 import { AuthProviderProps } from "react-oidc-context";
 
 export const oidcConfig: AuthProviderProps = {
-    authority: "http://localhost:8080/realms/my-realm",
-    client_id: "front-teste",
-    redirect_uri: "http://localhost:5173",
-    
+    authority: `http://localhost:${import.meta.env.VITE_KC_PORT}/realms/${import.meta.env.VITE_KC_REALM}`,
+    client_id: import.meta.env.VITE_KC_CLIENT_ID,
+    redirect_uri: import.meta.env.VITE_APP_URL,
+
     onSigninCallback: (): void => {
             window.history.replaceState(
                 {},
